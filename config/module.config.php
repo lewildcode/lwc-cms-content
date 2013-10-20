d1ad0a7810b1a9b74b38f6843064998d0d1df70d
Timo Ziemann <timoziemann@googlemail.com>
2013-10-20 19:54:29 +0200

    add view helper to config

--- config/module.config.php
+++ config/module.config.php
@@ -2,10 +2,14 @@
 return array(
     'view_helpers' => array(
         'invokables' => array(
+            // basic view helpers
             'contentHeader' => 'LwcCmsContent\View\Helper\Header',
             'contentImage' => 'LwcCmsContent\View\Helper\Image',
             'contentBodycopy' => 'LwcCmsContent\View\Helper\Bodycopy',
-            'contentArticle' => 'LwcCmsContent\View\Helper\Article'
+
+            // core content type view helpers
+            'contentArticle' => 'LwcCmsContent\View\Helper\Article',
+            'contentSection' => 'LwcCmsContent\View\Helper\Section'
         )
     ),
     'view_manager' => array(
@@ -13,12 +17,12 @@ return array(
             __DIR__ . '/../view'
         ),
         'template_map' => array(
-            // basics
+            // templates
             'content/header' => __DIR__ . '/../view/lwc-cms-content/content/header.phtml',
             'content/image' => __DIR__ . '/../view/lwc-cms-content/content/image.phtml',
             'content/bodycopy' => __DIR__ . '/../view/lwc-cms-content/content/bodycopy.phtml',
 
-            // core content types
+            // core content type templates
             'content/article' => __DIR__ . '/../view/lwc-cms-content/content/article.phtml',
             'content/section' => __DIR__ . '/../view/lwc-cms-content/content/section.phtml',
         )
